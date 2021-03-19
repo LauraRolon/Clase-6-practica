3cc6a977f3899b0f264d36fae273b69834e36c78
Laura Rolon <mrolonmartinez@udesa.edu.ar>
2021-03-19 17:37:04 -0300

    Consiga 1 lista

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.set('view engine','ejs')
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+
+
---
+++ bandas.js
@@ -0,0 +1,17 @@
+var bandas=[
+    {
+        nombre: "Arctic Monkeys",
+        topCanciones: [
+            "cancion 1",
+            "cancion 2",
+            "canción 3",
+        ]
+    }
+    {
+        banda 2,
+    }
+    {
+        banda 3,
+    }
+]
+module.exports=bandas
